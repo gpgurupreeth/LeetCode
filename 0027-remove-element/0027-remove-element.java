16b4c0a689f77525854ccb483c70fa3ed480d7e9
gpgurupreeth <gurugp2580@gmail.com>
2026-07-31 22:55:17 +0530

    Time: 0 ms (100%), Space: 43.4 MB (80.07%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -4,9 +4,12 @@ class Solution {
         int j=0;
         for(int i=0;i<nums.length;i++){
            if(nums[i]!=val){
-                int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
+                if(i!=j){
+                    int temp=nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                    
+                }
                 j++;
            }
         }
