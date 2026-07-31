124d2b3c6168c694b82e0be52951c356ae294e3c
gpgurupreeth <gurugp2580@gmail.com>
2026-07-31 22:35:17 +0530

    Time: 4 ms (1.31%), Space: 43.8 MB (8.99%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count=0;
+        int j=nums.length-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==val){
+                count++;
+                nums[i]=Integer.MAX_VALUE;
+                
+            }
+        }
+        Arrays.sort(nums);
+        return nums.length-count;
+
+    }
+}
