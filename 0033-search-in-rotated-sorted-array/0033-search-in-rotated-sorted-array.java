914021a33df05a1f973f1fe15ae1e1e799344b29
gpgurupreeth <gurugp2580@gmail.com>
2026-08-27 11:51:15 +0530

    Time: 0 ms (100%), Space: 43.8 MB (65.95%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -3,12 +3,11 @@ class Solution {
         int low=0;
         int high=nums.length-1;
         while(low<=high){
-            int mid=(low+high)/2;
+            int mid=low+(high-low)/2;
             if(nums[mid]==target){
                 return mid;
             }
             else if(nums[low]<=nums[mid]){
-                //left half is sorted
                 if(nums[low]<=target && nums[mid]>target){
                     high=mid-1;
                 }
@@ -17,17 +16,14 @@ class Solution {
                 }
             }
             else{
-                //right half is sorted
-                if(nums[mid] < target && target <= nums[high]){
-                    low = mid + 1;
+                if(nums[mid]<=target && nums[high]>=target){
+                    low=mid+1;
                 }
                 else{
-                    high = mid - 1;
+                    high=mid-1;
                 }
-                
             }
-            
-        }   
+        }
         return -1;
     }
 }
