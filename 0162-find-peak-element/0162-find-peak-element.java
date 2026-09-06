34e62536a3f5a25eb753bd96d95ed493d71c54a2
gpgurupreeth <gurugp2580@gmail.com>
2026-09-06 20:01:30 +0530

    Time: 0 ms (100%), Space: 44.4 MB (19.83%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -5,11 +5,10 @@ class Solution {
         }
         if(nums[0]>nums[1]){
             return 0;
-        }   
+        }
         if(nums[nums.length-1]>nums[nums.length-2]){
             return nums.length-1;
         }
-
         for(int i=1;i<=nums.length-2;i++){
             if(nums[i]>nums[i-1] && nums[i]>nums[i+1]){
                 return i;
