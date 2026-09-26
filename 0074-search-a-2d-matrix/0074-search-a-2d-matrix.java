5acb78b782ecf67e44793195d3d3a798626ed368
gpgurupreeth <gurugp2580@gmail.com>
2026-09-26 13:23:55 +0530

    Time: 0 ms (100%), Space: 44.1 MB (8.92%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,15 +1,10 @@
 class Solution {
 
     public boolean searchMatrix(int[][] matrix, int target) {
-
         int lowRow = 0;
         int highRow = matrix.length - 1;
-
-        // Find the possible row
         while (lowRow <= highRow) {
-
             int mid = lowRow + (highRow - lowRow) / 2;
-
             if (matrix[mid][0] <= target) {
                 lowRow = mid + 1;
             } 
@@ -17,22 +12,14 @@ class Solution {
                 highRow = mid - 1;
             }
         }
-
-        // highRow is now the last row whose first element <= target
         if (highRow < 0) {
             return false;
         }
-
         int row = highRow;
-
-        // Binary search inside the row
         int low = 0;
         int high = matrix[row].length - 1;
-
         while (low <= high) {
-
             int mid = low + (high - low) / 2;
-
             if (matrix[row][mid] == target) {
                 return true;
             } 
@@ -43,7 +30,6 @@ class Solution {
                 high = mid - 1;
             }
         }
-
         return false;
     }
 }
